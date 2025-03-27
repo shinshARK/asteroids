4f67473eb730c503a2e28cc3d871f954885d9c32
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-03-27 11:35:28 +0700

    refactor: use pygame groups

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
         # limit the framerate to 60 FPS
