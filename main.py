b087fed7554913aa5e108d2f05e4bac4c1400b15
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-03-27 09:36:37 +0700

    feat: add FPS limit to 60

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -14,6 +17,9 @@ def main():
                 return
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
