73e45f53fa3b9e807f21f373dd95595923c00083
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-03-27 10:18:37 +0700

    feat: add & draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,14 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
-        pygame.display.flip()
 
+        player.draw(screen)
+
+        pygame.display.flip()
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
