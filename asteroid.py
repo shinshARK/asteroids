242eb3dd8669c26239efc9dd28409a9f155c3c4c
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-03-27 13:09:50 +0700

    feat: add asteroid splitting

--- asteroid.py
+++ asteroid.py
@@ -1,9 +1,26 @@
 import pygame
+import random
 from circleshape import CircleShape
-
+from constants import ASTEROID_MIN_RADIUS
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        vec1, vec2 = self.velocity.rotate(random_angle), self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = vec1 * 1.2
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = vec2 * 1.2
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
